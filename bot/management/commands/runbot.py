05dfad26780fd6e91e072dcde2f55a4287245874
Glassyflute <yuliya.bobir@gmail.com>
2023-03-13 14:56:13 +0600

    lesson 38: Tg bot ensures authorization of user and writes user in DB with Telegram data and DB User data.

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -4,17 +4,9 @@ from django.core.management.base import BaseCommand
 
 from bot.models import TgUser
 from bot.tg.client import TgClient
-from bot.tg.dc import Message, GetUpdatesResponse, UpdateObj
+from bot.tg.dc import Message
 
 logger = logging.getLogger(__name__)
-
-################################################
-# Написать manage.py команду runbot для получения уведомлений от бота и отправки в ответ пользователю
-# текста, который был в уведомлении — то есть написать своего первого echo-бота.
-# После того, как реализуете получения уведомлений из tg (команда запускается, и вы в консоли
-# видите сообщения, которые пишете боту), нужно добавить отправку echo-сообщения обратно.
-# При запуске команды Python manage.py runbot бот начинает отправлять echo-сообщения в ответ
-# на пользовательские сообщения.
 
 
 class Command(BaseCommand):
@@ -24,53 +16,62 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         offset = 0
-        # logger.info("Bot started operating")
-        print("Bot started operating")
+        logger.info("Bot started operating")
 
         while True:
-            # res: GetUpdatesResponse = self.tg_client.get_updates(offset=offset)
             res = self.tg_client.get_updates(offset=offset)
             for item in res.result:
-                print(f"Original offset: {offset}")
-                # logger.info(item)
+                logger.info(f"UpdateObj is {item}")
                 offset = item["update_id"] + 1
-                print(f"Updated offset: {offset}")
-                # logger.info(item["message"])
-                print(item["message"])
+                message: Message = item["message"]
+                logger.info(f"Message is {message}")
 
-                # msg_chat_id = item["message"]["chat"]["id"]
-                # msg_echo_text = item["message"]["text"]
-                # self.tg_client.send_message(chat_id=msg_chat_id, text=msg_echo_text)
+                self.handle_message(message=message)
+                logger.info("Bot replied with message")
 
-                self.handle_echo_message(item["message"])
+    def handle_message(self, message: Message):
+        msg_chat_id: int = message["chat"]["id"]
 
-    def handle_echo_message(self, message: Message):
-        # msg_chat_id = message.chat.id
+        msg_echo_text: str = message["text"]
+        tg_username = message["chat"]["username"]
+
+        tg_user, created = TgUser.objects.get_or_create(tg_chat_id=msg_chat_id)
+        logger.info(f"Created: {created} for tg_chat_id <{msg_chat_id}>")
+
+        if tg_user.user:
+            self.handle_authorized_user(tg_user=tg_user, message=message)
+        else:
+            self.handle_unauthorized_user(tg_user=tg_user, message=message)
+
+        # self.tg_client.get_send_message(chat_id=msg_chat_id, text=msg_echo_text)
+
+    def handle_unauthorized_user(self, tg_user: TgUser, message: Message):
+        logger.info("User is not authorized. Need to generate Verification code and link "
+                    "the Telegram user with DB user via the Todolist web application.")
         msg_chat_id = message["chat"]["id"]
-        # msg_echo_text = message.text
-        msg_echo_text = message["text"]
+        greetings_text = f"Greetings, User. Please, verify your account. For proper authorization " \
+                         "insert the verification code provided by the Telegram bot into the application " \
+                         "while being logged in."
+        self.tg_client.get_send_message(chat_id=msg_chat_id, text=greetings_text)
 
-        # tg_user, created = TgUser.objects.get_or_create(tg_chat_id=msg_chat_id)
-        # print(f"Created: {created}")
-        #
-        # if tg_user.db_user:
-        #     print("User is authorized.")
-        # else:
-        #     print("User is not authorized. Please, insert the verification code provided by "
-        #           "the Telegram bot into the application while being logged in.")
+        tg_username = message["chat"]["username"]
+        updated_username = tg_user.assign_tg_username(username=tg_username)
 
-        self.tg_client.send_message(chat_id=msg_chat_id, text=msg_echo_text)
+        verification_code = tg_user.assign_verification_code()
+        verification_msg = f"Verification code: {verification_code}"
+        tg_user.save()
+
+        self.tg_client.get_send_message(chat_id=msg_chat_id, text=verification_msg)
+
+    def handle_authorized_user(self, tg_user: TgUser, message: Message):
+        logger.info("User is authorized.")
+        tg_username = message["chat"]["username"]
+        msg_chat_id = message["chat"]["id"]
+
+        msg_auth_greeting = f"Welcome to chat, {tg_username}"
+        self.tg_client.get_send_message(chat_id=msg_chat_id, text=msg_auth_greeting)
 
 
-# run polling
-#
-# verification_code_random = TgClient.objects.make_random_password(length=10,
-#                                                           allowed_chars="abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789")
-# # or for User model
-#
-# while TgClient.objects.filter(verification_code=verification_code_random):
-#     verification_code_random = TgClient.objects.make_random_password(length=10,
-#                                                                      allowed_chars="abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789")
-# TgClient.verification_code = verification_code_random
 
-# import telegram.ext
+
+    # for /start, /help === show original greeting message or list of functions
--- todolist/settings.py
+++ todolist/settings.py
@@ -171,47 +171,45 @@ BOT_TELEGRAM_TOKEN = env.str('BOT_TELEGRAM_TOKEN')
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
+    # 'filters': {
+    #     'require_debug_false': {
+    #         '()': 'django.utils.log.RequireDebugFalse',
+    #     },
+    #     'require_debug_true': {
+    #         '()': 'django.utils.log.RequireDebugTrue',
+    #     },
+    # },
     'formatters': {
-        'django.server': {
-            '()': 'django.utils.log.ServerFormatter',
-            'format': '[{server_time}] {message}',
-            'style': '{',
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(message)s',
         }
     },
     'handlers': {
         'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
+            # 'level': 'INFO',
+            # 'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
+            'formatter': 'console',
         },
-        'django.server': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'django.server',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'null': {'class': 'logging.NullHandler'},
+        # 'django.server': {
+        #     'level': 'INFO',
+        #     'class': 'logging.StreamHandler',
+        #     'formatter': 'console',
+        # },
+        # 'mail_admins': {
+        #     'level': 'ERROR',
+        #     'filters': ['require_debug_false'],
+        #     'class': 'django.utils.log.AdminEmailHandler'
+        # }
     },
     'loggers': {
-        'django': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'INFO',
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG' if DEBUG else 'INFO',
         },
         'django.server': {
-            'handlers': ['django.server'],
-            'level': 'INFO',
-            'propagate': False,
+            'handlers': ['null'],
         },
     }
 }
